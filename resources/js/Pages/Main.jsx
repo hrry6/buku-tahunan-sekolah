fe8fd1ef9e4951ccf28ce023214afdabf23fad76
rahyco rafly <rahycorafly@gmail.com>
2024-01-11 15:24:59 +0700

    rafly

--- resources/js/Pages/Main.jsx
+++ resources/js/Pages/Main.jsx
@@ -3,14 +3,15 @@ import { Link, Head } from '@inertiajs/react';
 export default function Main() {
     return (
         <>
-            <Head title="Buku Tahunan Sekolah" />
-            <p>Ini Main Page</p>
-            <Link
-                href={route('login')}
-                className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-            >
-                Login
-            </Link>
+       <div>
+      <h1 className="text-3xl font-bold ml-14 mt-10 border-b-2 border-black w-[570px]">REKAYASA PERANGKAT LUNAK B 2023</h1>
+      <h1 className="text-3x1 font-bold ml-14 mt-10 absolute right-[100px]">RPL B</h1>
+      <img 
+        src="/img/fotokelas.png"
+        alt="Foto Kelas"
+        className="block mx-auto mt-20"
+      />
+    </div>
         </>
     );
 }
